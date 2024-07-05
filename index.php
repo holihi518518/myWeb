a4ad133518ad926ba6b3f173e09216d3ac75601d
holihi518518 <98965108+holihi518518@users.noreply.github.com>
2024-07-06 01:00:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+    <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JKT9G0CBH8"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-JKT9G0CBH8');
+        </script>
+
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>宇澄生技公司產品推廣</title>
+        <link rel="stylesheet" href="style.css">
+
+        <link rel="icon" type="image/png" href="icon.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="icon.png" sizes="32x32">
+
+            
+        <!-- 社交媒体分享图片 -->
+        <meta property="og:title" content="宇澄生技公司產品推廣">
+        <meta property="og:description" content="探索我們的優質產品，讓您健康又美麗。">
+        <meta property="og:image" content="https://www.yucheng-mlm.com/wp-content/uploads/2024/04/424542.jpg">
+        <meta property="og:url" content="https://www.yucheng-mlm.com/wp-content/uploads/2024/04/424542.jpg">
+        <meta property="og:type" content="website">
+    </head>
+    
+<body>
+    <header>
+        <div id="visitor-count" class="visitor-count">歷史訪客數: <span id="visitor-count-number">0</span></div>
+
+        <div class="header-item logo-container">
+            <img src="https://www.yucheng-mlm.com/wp-content/uploads/elementor/thumbs/%E5%AE%87%E6%BE%84%E7%94%9F%E6%8A%80YU_logo-qo55ah7pwxrokpidpa4ljnkxa9ddo6n5ti4k57w6q4.jpg" alt="宇澄生技公司商標" class="logo">
+        </div>
+        <div class="header-item">
+            <a id="login-link" class="login-button">會員登入</a>
+        </div>
+    </header>
+
+    <!-- 跑馬燈 -->
+    <div class="marquee-container">
+        <div class="marquee">
+            <p>歡迎貴客蒞臨，探索宇澄優質產品，讓您健康又美麗。立即加入會員享受更多優惠！</p>
+        </div>
+    </div>        
+    <main>
+        <section id="product-section" class="content"></section>
+    </main>
+    <h3>~~更多優質商品都在宇澄官網喔，<a href="https://reurl.cc/qVjoNq" target="_blank">點我進入</a></h3>
+    <footer>
+        <div class="footer-info">            
+            <div class="company-info">
+                <h3>宇澄生技國際有限公司</h3>
+                <p>Yucheng Biotechnology Co., Ltd.</p>
+                <p>統一編號：93577400</p>
+                <p>電話：04-24391234 ; 04-22398617#11</p>
+                <p>傳真：04-22398717</p>
+                <p>地址：台中市北屯區環中東路二段125-5號</p>
+                <p>營業時間：AM09:30~PM18:30</p>
+            </div>
+            <div class="contact-info">
+                <a>聯絡信箱：yoyo7485@yahoo.com.tw</a>
+                <a>LINE聯絡呂小姐了解更多資訊</a>
+                <a href="https://line.me/ti/p/@yucheng"><img src="line.jpg" width="65" height="65" alt="LINE聯絡呂小姐"></a>                
+                <a>推薦人代碼：TW22623623</a>
+            </div>            
+        </div>
+        
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
